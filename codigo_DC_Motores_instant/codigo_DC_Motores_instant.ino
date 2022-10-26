040e7fecbd0e2f4ef747621b64d3c50581d430d7
Thanos002 <uyiky@student.kit.edu>
2022-10-26 19:44:48 +0200

    26/10

--- codigo_DC_Motores_instant/codigo_DC_Motores_instant.ino
+++ codigo_DC_Motores_instant/codigo_DC_Motores_instant.ino
@@ -21,19 +21,19 @@
 #define STEP 30
 #define DIR 31
 
-// pins del stepper driver 
-#define SRESET 32    // resets internal logic and step table, defualt inactive/low during operation
-#define SLEEP 33  // low power mode, needs 1ms to reactivate
-#define ENAPIN 34    // Enables outputs, defualt inactive/low during operation
+// pins del stepper driver
+#define SRESET 32  // resets internal logic and step table, defualt inactive/low during operation
+#define SLEEP 33   // low power mode, needs 1ms to reactivate
+#define ENAPIN 34  // Enables outputs, defualt inactive/low during operation
 
 // para micropasos del stepper:
 #define MS1 35
 #define MS2 36
 #define MS3 37
 
-/pins de los servos
-#define SERVO1 38 // servo girar
-#define SERVO2 39 // servo abrir
+//pins de los servos
+#define SERVO1 38  // servo girar
+#define SERVO2 39  // servo abrir
 
 // pins de fin de carrera (p.e. FINDE fin de motor del extremo (izquierdo) a la posicion derecha)
 #define FINDE 40
@@ -60,10 +60,13 @@ int pulsosi = 0;
 const int ratio = 6316;  //1579*4
 int iterations = 0;
 
+// numero de pulsos/grados maximos
 const int DGRADOSMAX = 190;
 const int IGRADOSMAX = 280;
-const int DPULSOSMAX = (float)DGRADOSMAX*(float)(ratio/360)*4.5; //15000
-const int IPULSOSMAX = (float)IGRADOSMAX*(float)(ratio/360)*7.281125; //22106
+const int DPULSOSMAX = (float)DGRADOSMAX * (float)(ratio / 360) * 4.5;       //15000
+const int IPULSOSMAX = (float)IGRADOSMAX * (float)(ratio / 360) * 7.281125;  //22106
+const int ZMMMAX = 270;
+const int ZPULSOSMAX = 270;
 
 //int contador = 0;
 //int contador2 = 0;
@@ -74,9 +77,6 @@ float prevalto;
 bool pinza;
 
 // numero maximo de pasos/pulsos
-int maxD;
-int maxR;
-int maxS;
 
 volatile int posid = 0;  // specify posi as volatile
 long prevT = 0;
@@ -87,40 +87,40 @@ float eprevi = 0;
 float eintegrali = 0;
 int giro;
 
+// init servos
 Servo girar;
 Servo abrir;
 
-home()
-float home(int motorpin1, int motorpin2, int finpin, int finpin2, int PWMpin, int pos, int posi, int DPULSOSMAX){
+// homing functions
+float home(int motorpin1, int motorpin2, int finpin, int finpin2, int PWMpin, int posi, int DPULSOSMAX) {
   analogWrite(PWMpin, 30);
-  posprev=pos;
-  while(digitalRead(finpin)==0){
+  posprev = posi;
+  while (digitalRead(finpin) == 0) {
     digitalWrite(motorpin1, HIGH);
     digitalWrite(motorpin2, LOW);
   }
   digitalWrite(motorpin1, LOW);
   digitalWrite(motorpin2, LOW);
-  
-  float delta = fabs(posprev-posi);
-  posi = posi-DPULSOSMAX/2;
 
+  float delta = fabs(posprev - posi);
+  posi = -DPULSOSMAX / 2;
 }
 
-float homestepper(int dirpin, int steppin, int finpin){
-  while(digitalRead(finpin)==0){
-    digitalWrite(dirpin,LOW);
-    digitalWrite(steppin,HIGH); 
-    delayMicroseconds(500); 
-    digitalWrite(steppin,LOW); 
-    delayMicroseconds(500); 
+float homestepper(int dirpin, int steppin, int finpin) {
+  while (digitalRead(finpin) == 0) {
+    digitalWrite(dirpin, LOW);
+    digitalWrite(steppin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(steppin, LOW);
+    delayMicroseconds(500);
   }
   digitalWrite(steppin, LOW);
-  digitalWrite(steppin, LOW);
+  digitalWrite(SLEEP, HIGH);
 
-  alto =0;
+  alto = 0;
+}
 
-  }
-
+// BEGIN OF SETUP
 
 void setup() {
   Serial.begin(9600);
@@ -164,9 +164,10 @@ void setup() {
   pinMode(FINZB, INPUT_PULLUP);
   pinMode(FINZA, INPUT_PULLUP);
 
-  homestepper();
-  home();
- 
+  // homing of all motors
+  home(DCD_1, DCD_2, FINII, FINDI, PWMD, posid, DPULSOSMAX);
+  home(DCI_1, DCI_2, FINIE, FINDE, PWMI, posii, IPULSOSMAX);
+  homestepper(DIR, STEP, FINZB);
 }
 
 void loop() {
@@ -179,14 +180,13 @@ void loop() {
     gradd = (int)Serial.readStringUntil(',').toInt();
     gradi = (int)Serial.readStringUntil(',').toInt();
     alto = (int)Serial.readStringUntil(',').toInt();
-    giro = (int)Serial.readStringUntil(',').toInt(); 
+    giro = (int)Serial.readStringUntil(',').toInt();
     pinza = (int)Serial.readStringUntil('\n').toInt();
-    pulsosd = signCorrection(dird, gradd);
-    pulsosi = signCorrection(diri, gradi);
+    pulsosd = Dgrados2pulsos(signCorrection(dird, gradd));
+    pulsosi = Igrados2pulsos(signCorrection(diri, gradi));
 
     setStepper(alto);
     setPinza(giro, pinza);
-
   }
 
   // PID constants
@@ -255,7 +255,7 @@ void loop() {
   float pwrd = setPower(ud);
   float pwri = setPower(ui);
 
-    // motor direction
+  // motor direction
   int dird = setDir(dird, ud);
   int diri = setDir(diri, ud);
 
@@ -263,25 +263,20 @@ void loop() {
   setMotor(dird, pwrd, PWMD, DCD_1, DCD_2);
   setMotor(diri, pwri, PWMI, DCI_1, DCI_2);
 
-  // store previous error
+  // store previous errors
   eprevd = ed;
   eprevi = ei;
 }
 
+// replace opcode with sign 
 int signCorrection(int dir, int grad) {
-  int pulsos;
-  if (grad >= 0 && grad <= 360) {
-    pulsos = (int)((float)grad * (float)((float)ratio / (float)360));
-  }
   if (dir == 0) {
-    pulsos = -pulsos;
-  } else {
-    //Serial.print("Valor por encima de 360º");
-    return 0;
+    grad = -grad;
+    return grad;
   }
-  return pulsos;
 }
 
+// function to set motor for PID, argument direction and pwm
 void setMotor(int dir, int pwmVal, int pwm, int pin1, int pin2) {
   //set direction and speed of motor
   analogWrite(pwm, pwmVal);
@@ -294,44 +289,40 @@ void setMotor(int dir, int pwmVal, int pwm, int pin1, int pin2) {
   } else {
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, LOW);
-    if(pwm == PWMD){
+    if (pwm == PWMD) {
       dird = 0;
       pulsosd = 0;
-    }
-    else{
+    } else {
       diri = 0;
       pulsosi = 0;
     }
-
   }
 }
 
-void setStepper(int steps){
-  digitalWrite(SLEEP,0);
+void setStepper(int steps) {
+  digitalWrite(SLEEP, 0);
   delay(1);
-  if(steps-prevalto>0){
+  if (steps - prevalto > 0) {
     digitalWrite(DIR, HIGH);
+  } else {
+    digitalWrite(DIR, LOW);
   }
-  else{
-    digitalWrite(DIR,LOW);
-  }
-  for(int x = 0; x < steps; x++) {
-    digitalWrite(STEP,HIGH); 
-    delayMicroseconds(500); 
-    digitalWrite(STEP,LOW); 
-    delayMicroseconds(500); 
+  for (int x = 0; x < steps; x++) {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(500);
   }
   delay(500);
-  digitalWrite(SLEEP,1);
-  prevalto=steps;
+  digitalWrite(SLEEP, 1);
+  prevalto = steps;
 }
 
-void setPinza(int grad, bool open){
+void setPinza(int grad, bool open) {
   girar.write(grad);
-  if (open==0){
+  if (open == 0) {
     abrir.write(36);
-  }
-  else{
+  } else {
     abrir.write(0);
   }
 }
@@ -425,21 +416,54 @@ void readEncoderIBF() {  //when A Rising or B Falling
   }
 }
 
-//conversion de grados a pulsos del motor derecho /brazo interno
-float Dgrados2pulsos(float grados){
-  return (float)grados*(float)(ratio/360)*4.5;
+// conversion de grados a pulsos del motor derecho /brazo interno
+// comprobar si el punto esta dentro el campo de trabajo
+float Dgrados2pulsos(float grados) {
+  float result = (float)grados * (float)(ratio / 360) * 4.5;
+  if ((result < DPULSOSMAX / 2 - posid) && (result > posid - DPULSOSMAX / 2)) {
+    return result;
+  } else {
+    Serial.print(4);
+    Serial.print(",");
+    Serial.print(1);
+    Serial.print(",");
+    Serial.print(result);
+    Serial.print(",");
+    Serial.println(posid);
+    return 0;
+  }
 }
 
-// motor izquierdo
-float Igrados2pulsos(float grados){
-  return (float)grados*(float)(ratio/360)*7.281125;
+// lo igual, para el motor izquierdo
+float Igrados2pulsos(float grados) {
+  float result = (float)grados * (float)(ratio / 360) * 7.281125;
+  if ((result < IPULSOSMAX / 2 - posii) && (result > posii - IPULSOSMAX / 2)) {
+    return result;
+  } else {
+    Serial.print(4);
+    Serial.print(",");
+    Serial.print(2);
+    Serial.print(",");
+    Serial.print(result);
+    Serial.print(",");
+    Serial.println(posii);
+    return 0;
+  }
 }
 
-// stepper
-float mm2Pulsos(float mm){
-  return (float)mm*100;
-}
-
-
-
+// stepper conversion
+float mm2Pulsos(float mm) {
+  float result = (float)mm * 100;
+  if (result < 0 or result > 270) {
+    return result;
+  } else {
+    Serial.print(4);
+    Serial.print(",");
+    Serial.print(3);
+    Serial.print(",");
+    Serial.print(result);
+    Serial.print(",");
+    Serial.println(alto);
+    return 0;
+  }
 }
